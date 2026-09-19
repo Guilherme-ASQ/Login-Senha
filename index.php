3d5048852a854585528245eb584cb1697af0cc49
Guilherme-ASQ <150046840+Guilherme-ASQ@users.noreply.github.com>
2026-09-19 11:33:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<DOCTYPE html!>
+<!DOCTYPE html>
 <html lang="pt-BR">
     <head>
         <meta charset="UTF-8">
@@ -38,6 +38,16 @@
                     <input type="password" name="novaSenha" id="novaSenha" placeholder="Digite uma senha" required>
                 </div>
 
+                <!--Adicionei coisas novas no cadastro. Agora tem que ter também o email.
+                No BAnco de Dados do NEON, tem o tipo do usuario, que pode ser ou ADMINISTRADOR ou CLIENTE.
+                Pro cadastro, eu vou sempre passar o tipo do usuario como CLIENTE. Pq tecnicamente serão poucos administradores no sistema.
+                Caso eu precise adicionar um novo administrador, eu mudo isso diretamente no banco de dados.
+                -->
+                <div class="input-group">
+                    <label for="novoEmail">Novo Email:</label>
+                    <input type="email" name="novoEmail" id="novoEmail" placeholder="Digite um email" required>
+                </div>
+
                 <button type="submit">CADASTRAR</button>
             </form>
 
@@ -65,16 +75,21 @@
 
                 //Verifica qual o formulário foi enviado 
                     //Formulario de Cadastro
-                    if(isset($_POST["novoUsuario"]) && isset($_POST["novaSenha"]) ){
+                    if(isset($_POST["novoUsuario"]) && isset($_POST["novaSenha"]) && isset($_POST["novoEmail"]) ){
                         //Pega os dados digitados no formulário
                         $novoUsuario = $_POST["novoUsuario"];
                         $novaSenha = $_POST["novaSenha"];
+                        $novoEmail = $_POST["novoEmail"];
+                        //COmo eu vou cadastrar os usuarios como CLIENTE, pq terão poucos administradores,
+                        //por padrão todos os clientes serão cadastrados como CLIENTE. Caso precise, eu
+                        //mudo no banco de dados mesmo, com os comandos SQL.
+                        $tipoUsuario = "CLIENTE";
 
                         //Insere o novo usuario no banco
                         $resultado = pg_query_params(
                             $conexao,
-                            "INSERT INTO usuarios (nome, senha) VALUES ($1, $2)",
-                            array($novoUsuario, $novaSenha)
+                            "INSERT INTO usuarios (nome, senha, email, tipo_usuario) VALUES ($1, $2, $3, $4)",
+                            array($novoUsuario, $novaSenha, $novoEmail, $tipoUsuario)
                         );
 
                         //Verifica se o cadastro funcionou
